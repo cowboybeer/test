66a1b0fa2ce16cc6aa3e95399f1234a67125ef49
cowboybeer <misterybeer@gmail.com>
2022-01-06 16:46:21 +0100

    Create MoreRecursion.js

---
+++ MoreRecursion.js
@@ -0,0 +1,81 @@
+/*
+ *
+ * 1. write a recursive function that multiplies all numbers in the array
+ * 2. write a recursive function that multiplies all numbers in the array, if any number in the array is negative, multiply it as a positive number
+ * 3. write a recursive function that finds the largest number in the array
+ * 4. write a recursive function that finds the first number between two given numbers a and b: findBetween(array, a, b)
+ * 5. write a function that returns the n-th number on the 'funky series'. The 'funky series' is defined as follows:
+ *   1st number F_1: 3
+ *   2nd number F_2: 5
+ *   any other number: 11 + ((F_(n-1)^2) / F_(n-2))
+ *   (F_(n-1) squared divided by F_(n-2) -- then add 11 to the result
+ *
+ *   where F_(n-1) is the previous number on the 'Funky series', and F_(n-2) is the number before that
+ */
+
+// 1. write a recursive function that multiplies all numbers in the array
+let arr = [2, 4, 7, 8, 9, 10];
+let arrSize = arr.length;
+
+function multiplyThemAll(arr, size) {
+  let all = arr[size - 1];
+  if (size === 1) {
+    return all;
+  }
+  return all * multiplyThemAll(arr, size - 1);
+}
+
+console.log(multiplyThemAll(arr, arrSize));
+
+// 2. write a recursive function that multiplies all numbers in the array,
+// if any number in the array is negative, multiply it as a positive number
+let arr2 = [8, 4, -6, 7];
+let arr2Size = arr2.length;
+
+function multiplyThem(arr, size) {
+  let num = arr[size - 1];
+
+  if (num < 0) {
+    num *= -1;
+  }
+
+  if (size === 1) {
+    return num;
+  }
+
+  return num * multiplyThem(arr, size - 1);
+}
+
+console.log(multiplyThem(arr2, arr2Size));
+
+// 3. write a recursive function that finds the largest number in the array
+let largeArr = [23, 546, 337, 6, 879, 54, 34, 343, 646];
+let largeArrSize = largeArr.length;
+
+function largestNum(arr, size) {
+  let current = arr[size - 1];
+  console.log(current);
+  if (size === 1) {
+    return current;
+  } else {
+    let maxOfRest = largestNum(arr, size - 1);
+    console.log(maxOfRest);
+    if (current > maxOfRest) {
+      console.log(current);
+      return current;
+    } else if (maxOfRest > current) {
+      console.log(maxOfRest);
+      return maxOfRest;
+    }
+  }
+}
+
+console.log(largestNum(largeArr, largeArrSize));
+
+// 4. write a recursive function that finds the first number between two given
+//  numbers a and b: findBetween(array, a, b)
+let oneArray = [24, 476, 87, 34, 367, 578, 232, 656, 21];
+
+function findBetween(arr, a, b) {}
+
+console.log(findBetween(oneArray, 87, 232));
